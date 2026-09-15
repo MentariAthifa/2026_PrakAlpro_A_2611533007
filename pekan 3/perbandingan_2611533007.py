ba895ab0488f7daa2ec11fdb04ce0820543d86c8
MentariAthifa <mentariathifaa@gmail.com>
2026-09-15 17:10:16 +0700

    Add files via upload

---
+++ pekan 3/perbandingan_2611533007.py
@@ -0,0 +1,40 @@
+angka1_3007 = int(input("Input angka-1: "))
+angka2_3007 = int(input("Input angka-2: "))
+
+#Lebih  besar dari
+hasil = angka1_3007 > angka2_3007
+print ("\nOperator lebih besar dari")
+print("angka-1 > angka-2 =", hasil)
+
+#Lebih kecil dari
+hasil = angka1_3007 < angka2_3007
+print ("\nOperator lebih kecil dari")
+print("angka-1 < angka-2 =", hasil)
+
+#Lebih besar dari atau sama dengan
+hasil = angka1_3007 >= angka2_3007
+print ("\nOperator lebih besar dari atau sama dengan")
+print("angka-1 >= angka-2 =", hasil)
+
+#Lebih kecil dari atau sama dengan
+hasil = angka1_3007 <= angka2_3007
+print("\nOperator lebih kecil dari atau sama dengan")
+print("angka-1 <= angka-2 =", hasil)
+
+#Sama dengan
+hasil = angka1_3007 == angka2_3007
+print("\nOperator sama dengan")
+print("angka-1 == angka-2 =", hasil)
+
+#Tidak sama dengan
+hasil = angka1_3007 != angka2_3007
+print("\nOperator tidak sama dengan")
+print("angka-1 != angka-2 =", hasil)
+
+#Tambahan : perbandingan berantai dalam Python
+hasil = 0 < angka1_3007 < 100
+print("\nPerbandingan berantai") 
+print("0 < angka-1 < 100 =", hasil)
+
+hasil = 0 < angka2_3007 < 100
+print("0 < angka-2 < 100 =", hasil)
