c83723c87b517056f9585c11ffd306911fb2ec98
MentariAthifa <mentariathifaa@gmail.com>
2026-09-17 12:25:13 +0700

    Add files via upload

--- pekan 3/aritmatika_2611533007.py
+++ pekan 3/aritmatika_2611533007.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 angka1_3007 = int(input("Input angka-1: "))
 angka2_3007 = int(input("Input angka-2: "))
 
@@ -37,44 +36,3 @@ else:
 hasil = angka1_3007 ** angka2_3007
 print ("\nOperator Pangkat")
 print ("Hasil =", hasil)
-
-=======
-angka1_3007 = int(input("Input angka-1: "))
-angka2_3007 = int(input("Input angka-2: "))
-
-#Penjumlahan
-hasil = angka1_3007 + angka2_3007
-print ("\nOperator Penjumlahan")
-print ("Hasil =", hasil)
-
-#Pengurangan
-hasil = angka1_3007 - angka2_3007
-print ("\nOperator Pengurangan")
-print ("Hasil =", hasil)
-
-#Perkalian
-hasil = angka1_3007 * angka2_3007
-print ("\nOperator Perkalian")
-print ("Hasil =", hasil)
-
-#Pembagian, pembagian bulat, dan sisa bagi
-if angka2_3007 != 0:
-    hasil = angka1_3007 / angka2_3007
-    print ("\nOperator Pembagian")
-    print ("Hasil =", hasil)
-
-    hasil = angka1_3007 // angka2_3007
-    print ("\nOperator Pembagian Bulat")
-    print ("Hasil =", hasil)
-
-    hasil = angka1_3007 % angka2_3007
-    print ("\nOperator Sisa Bagi")
-    print ("Hasil =", hasil)
-else:
-    print("Angka kedua tidak boleh nol.")
-
-#Pangkat
-hasil = angka1_3007 ** angka2_3007
-print ("\nOperator Pangkat")
-print ("Hasil =", hasil)
->>>>>>> 45cc1ded8838115dacd16d20aa506ab9607d4717
